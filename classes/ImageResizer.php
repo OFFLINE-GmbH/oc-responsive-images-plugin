29ef70905a9b248dcc5e1c47d8df74d0b1c901b3
Tobias Kündig <tobias@offline.swiss>
2016-10-28 18:48:14 +0200

    Added compatibility with current edgeUpdate builds

--- classes/ImageResizer.php
+++ classes/ImageResizer.php
@@ -15,6 +15,14 @@ class ImageResizer extends Resizer
      */
     public function getWidth()
     {
+        // This call is needed since Build 370 when the new getWidth
+        // method was introduced and conflicted with this implementation.
+        // We can now use the parent's getWidth method if it is available.
+        // For older versions of october the width property gets returned.
+        if (is_callable('parent::getWidth')) {
+            return parent::getWidth();
+        }
+
         return $this->width;
     }
 }
