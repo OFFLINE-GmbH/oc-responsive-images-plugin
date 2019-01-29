7b10cf3d1a6f3e27df627598714bfdd74bd58c29
Michael Tellenbach <michael.tellenbach@gmail.com>
2019-01-29 13:50:59 +0100

    Bugfix: inner wrap the backend-image and center for optimal point scalation of smaller images

--- widgets/fileupload/assets/js/focuspoint-tool.js
+++ widgets/fileupload/assets/js/focuspoint-tool.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
     initFocuspointTool()
     const observableNode = document.querySelector('.upload-files-container')
-    const observerConfig = {attributes: true, childList: true, subtree: true}
+    const observerConfig = {childList: true, subtree: true}
     let backendObserver = new MutationObserver(initFocuspointTool)
     backendObserver.observe(observableNode, observerConfig)
 })
@@ -14,32 +14,35 @@ function initFocuspointTool () {
     Array.prototype.forEach.call(uploadedImages, el => {
         // Click on the uploaded image will open the customized config form
         el.addEventListener('click', function () {
-
-            // Observe the document for changes like modal popups
-            const observerConfig = {attributes: true, childList: true, subtree: true}
+            // Observe the document for modal popup
+            const observerConfig = {childList: true, subtree: true}
             let modalObserver = new MutationObserver(function () {
                 let fileuploadConfigForm = document.querySelector('.fileupload-config-form')
 
                 // Check if fileupload config form was loaded
                 if (fileuploadConfigForm) {
 
-                    let el = fileuploadConfigForm.querySelector('.img-responsive')
+                    let img = fileuploadConfigForm.querySelector('.img-responsive')
                     let point = fileuploadConfigForm.querySelector('.focuspoint')
 
                     // Load image dimensions
                     focuspointImage.dimensions = el.getBoundingClientRect()
 
-                    // Set focuspoint-attributes to default x=0, y=0 (center, center)
+                    // Set focuspoint-attributes to default x=50, y=50 (center, center)
                     focuspointImage.focuspoint = {
                         x: document.getElementById('x-axis').value || 50,
                         y: document.getElementById('y-axis').value || 50
                     }
 
+                    // Set the initial values to Axis-Fields
+                    document.getElementById('x-axis').value = focuspointImage.focuspoint.x
+                    document.getElementById('y-axis').value = focuspointImage.focuspoint.y
+
                     // Set the point to the center of the image
                     point.style.left = focuspointImage.focuspoint.x + '%'
                     point.style.top = focuspointImage.focuspoint.y + '%'
 
-                    el.addEventListener('click', function (e) {
+                    img.addEventListener('click', function (e) {
 
                         // Reload dimensions to prevent loading issues
                         focuspointImage.dimensions = el.getBoundingClientRect()
@@ -54,7 +57,7 @@ function initFocuspointTool () {
                             y: 100 / focuspointImage.dimensions.height * e.offsetY
                         }
 
-                        // Set values to Axis-Fields
+                        // Update values to Axis-Fields
                         document.getElementById('x-axis').value = focuspointImage.focuspoint.x
                         document.getElementById('y-axis').value = focuspointImage.focuspoint.y
                     })
