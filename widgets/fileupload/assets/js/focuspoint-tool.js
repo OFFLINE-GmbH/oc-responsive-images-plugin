dc45177f434a6f062ec8a52735f777f567b1fa07
Tobias Kündig <tobias@offline.swiss>
2019-10-05 10:39:22 +0200

    Refactored focuspoint-tool.js

--- widgets/fileupload/assets/js/focuspoint-tool.js
+++ widgets/fileupload/assets/js/focuspoint-tool.js
@@ -1,81 +1,30 @@
 window.addEventListener('load', function () {
     initFocuspointTool()
-    var observableNode = document.querySelector('.upload-files-container') ? document.querySelector('.upload-files-container') : document.querySelector('.rowlink')
+    var observableNode = document.querySelector('.upload-files-container')
+    if (!observableNode) {
+        observableNode = document.querySelector('.rowlink')
+    }
+    if (!observableNode) {
+        return;
+    }
+
     var observerConfig = {childList: true, subtree: true}
     var backendObserver = new MutationObserver(initFocuspointTool)
     backendObserver.observe(observableNode, observerConfig)
 })
 
 function initFocuspointTool () {
-
     var uploadedImages = document.querySelectorAll('.upload-object')
     var rowlinks = document.querySelectorAll('.rowlink')
 
-    var focuspointImage = {}
-
     if (uploadedImages.length > 0) {
-        Array.prototype.forEach.call(uploadedImages, function(el) {
-                // Click on the uploaded image will open the customized config form
-                el.addEventListener('click', function() {
-                    // Observe the document for modal popup
-                    var observerConfig = {childList: true, subtree: true}
-                    var modalObserver = new MutationObserver(function() {
-                        var fileuploadConfigForm = document.querySelector('.fileupload-config-form')
-
-                        // Check if fileupload config form was loaded and foucspoint is enabled (x-axis field)
-                        if (fileuploadConfigForm && document.getElementById('x-axis')) {
-
-                            var img = fileuploadConfigForm.querySelector('.img-responsive')
-                            var point = fileuploadConfigForm.querySelector('.focuspoint')
-
-                            // Load image dimensions
-                            focuspointImage.dimensions = img.getBoundingClientRect()
-
-                            // Set focuspoint-attributes to default x=50, y=50 (center, center)
-                            focuspointImage.focuspoint = {
-                                x: document.getElementById('x-axis').value || 50,
-                                y: document.getElementById('y-axis').value || 50
-                            }
-
-                            // Initial set values to Axis-Fields
-                            document.getElementById('x-axis').value = focuspointImage.focuspoint.x
-                            document.getElementById('y-axis').value = focuspointImage.focuspoint.y
-
-                            // Set the point to the center of the image
-                            point.style.left = focuspointImage.focuspoint.x + '%'
-                            point.style.top = focuspointImage.focuspoint.y + '%'
-
-                            img.addEventListener('click', function (e) {
-
-                                // Reload dimensions to prevent loading issues
-                                focuspointImage.dimensions = img.getBoundingClientRect()
-
-                                // Move the point to the clicked area
-                                point.style.left = e.offsetX + 'px'
-                                point.style.top = e.offsetY + 'px'
-
-                                // Set the focuspoint values
-                                focuspointImage.focuspoint = {
-                                    x: 100 / focuspointImage.dimensions.width * e.offsetX,
-                                    y: 100 / focuspointImage.dimensions.height * e.offsetY
-                                }
-
-                                // Update values to Axis-Fields
-                                document.getElementById('x-axis').value = focuspointImage.focuspoint.x
-                                document.getElementById('y-axis').value = focuspointImage.focuspoint.y
-                            })
-                        }
-                    })
-
-                    // Trigger observer
-                    modalObserver.observe(document, observerConfig)
-                })
-            }
-        )
+        Array.prototype.forEach.call(uploadedImages, function (el) {
+            el.addEventListener('click', onImageClick)
+        })
     } else {
         // If multi modal reverse initFocuspoint
-        Array.prototype.forEach.call(rowlinks, function(el) {
-            el.addEventListener('click', function() {
+        Array.prototype.forEach.call(rowlinks, function (el) {
+            el.addEventListener('click', function () {
                 // Observe the document for modal popup
                 var observerConfig = {childList: true, subtree: true}
                 var modalObserver = new MutationObserver(initFocuspointTool)
@@ -86,3 +35,72 @@ function initFocuspointTool () {
     }
 }
 
+/**
+ * Event handler for images in the upload widget.
+ */
+function onImageClick () {
+    waitForConfigForm(0, function () {
+        // Abort if the OFFLINE.ResponsiveImages fields are missing.
+        if (!document.getElementById('x-axis')) {
+            return
+        }
+
+        var fileuploadConfigForm = document.querySelector('.fileupload-config-form')
+        var img = fileuploadConfigForm.querySelector('.img-responsive')
+        var point = fileuploadConfigForm.querySelector('.focuspoint')
+        var xAxisInput = document.getElementById('x-axis')
+        var yAxisInput = document.getElementById('y-axis')
+
+        var focuspointImage = {
+            dimensions: img.getBoundingClientRect(),
+            focuspoint: {
+                x: xAxisInput.value || 50,
+                y: yAxisInput.value || 50
+            }
+        }
+
+        // Set initial values.
+        xAxisInput.value = focuspointImage.focuspoint.x
+        yAxisInput.value = focuspointImage.focuspoint.y
+
+        // Set marker position.
+        point.style.left = focuspointImage.focuspoint.x + '%'
+        point.style.top = focuspointImage.focuspoint.y + '%'
+        point.style.display = 'block'
+
+        img.addEventListener('click', function (e) {
+            // Reload dimensions to prevent loading issues
+            focuspointImage.dimensions = img.getBoundingClientRect()
+
+            // Move the point to the clicked area
+            point.style.left = e.offsetX + 'px'
+            point.style.top = e.offsetY + 'px'
+
+            // Set the focuspoint values
+            focuspointImage.focuspoint = {
+                x: 100 / focuspointImage.dimensions.width * e.offsetX,
+                y: 100 / focuspointImage.dimensions.height * e.offsetY
+            }
+
+            // Update values to Axis-Fields
+            xAxisInput.value = focuspointImage.focuspoint.x
+            yAxisInput.value = focuspointImage.focuspoint.y
+        })
+    })
+}
+
+/**
+ * Wait until the config form is added to the DOM.
+ */
+function waitForConfigForm (tries, callback) {
+    if (document.querySelector('.fileupload-config-form')) {
+        return callback()
+    }
+    if (tries >= 10) {
+        console.error('[OFFLINE.ResponsiveImages] Config form failed to load.')
+        return false
+    }
+    setTimeout(function () {
+        waitForConfigForm(++ tries, callback)
+    }, 500)
+}
