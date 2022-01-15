b492998400b43b10359cfdb0e6d03f2c03d7f612
Tobias Kündig <tobias@offline.ch>
2022-01-15 09:53:58 +0100

    Revert "Using Media\Classes\MediaLibrary (#86)"
    This reverts commit 50fddc882ebde1e854d0c1f19d39bddf5e05a8c2.

--- classes/ResponsiveImage.php
+++ classes/ResponsiveImage.php
@@ -10,7 +10,7 @@ use OFFLINE\ResponsiveImages\Classes\Exceptions\FileNotFoundException;
 use OFFLINE\ResponsiveImages\Classes\Exceptions\RemotePathException;
 use OFFLINE\ResponsiveImages\Classes\Exceptions\UnallowedFileTypeException;
 use OFFLINE\ResponsiveImages\Models\Settings;
-use Media\Classes\MediaLibrary;
+use System\Classes\MediaLibrary;
 use URL;
 use WebPConvert\WebPConvert;
 
